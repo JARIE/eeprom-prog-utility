1f7d8c46bf0e2ec64bd4a84e8c9e1f713401eabc
Jarielle Catbagan <jrcatbagan@ca.rr.com>
2014-08-20 23:46:27 -0700

    added GPL v3.0

--- programmer/mem.c
+++ programmer/mem.c
@@ -1,6 +1,22 @@
 // File: mem.c
-// Author: Jarielle Catbagan
 // Created: 30, May 2014
+
+/* Copyright (C) 2014 Jarielle Catbagan
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
 
 #include <avr/io.h>
 #define F_CPU 20000000UL
@@ -49,11 +65,9 @@ void mem_write(uint16_t address, uint8_t data) {
         _delay_us(1);
         sst_WEhigh();
 
-        _delay_us(5);
+        _delay_us(30);
         sst_CEhigh();
         _delay_us(1);
-
-        sst_datapoll(data);
                 
         sst_clearaddr(); 
         sst_cleardata();
